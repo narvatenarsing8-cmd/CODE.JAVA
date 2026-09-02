e4e03f30bfebd4758ab0d1283a9c6f309fb8cb72
Narsing Narvate <narvatenarsing8@gmail.com>
2026-09-02 10:18:52 +0530

    Add Java program to check positive, negative, or zero

---
+++ PositiveNegative.java
@@ -0,0 +1,16 @@
+public class PositiveNegative {
+    public static void main(String[] args) {
+
+        int num = 10;
+
+        if (num > 0) {
+            System.out.println("Positive");
+        } 
+        else if (num < 0) {
+            System.out.println("Negative");
+        } 
+        else {
+            System.out.println("Zero");
+        }
+    }
+}
