8cc6dc93ee9b734662059688c1f29b9b2b81524f
Narsing Narvate <narvatenarsing8@gmail.com>
2026-09-02 10:20:25 +0530

    Add Java program to identify person category by age

---
+++ PersonCategory.java
@@ -0,0 +1,25 @@
+public class PersonCategory {
+    public static void main(String[] args) {
+
+        int age = 25;
+
+        if (age >= 3 && age < 13) {
+            System.out.println("You are a child");
+        } 
+        else if (age >= 13 && age < 19) {
+            System.out.println("You are a teenager");
+        } 
+        else if (age >= 19 && age <= 40) {
+            System.out.println("You are an adult");
+        } 
+        else if (age > 40 && age <= 60) {
+            System.out.println("You are middle aged");
+        } 
+        else if (age > 60) {
+            System.out.println("You are a senior citizen");
+        } 
+        else {
+            System.out.println("You are a baby");
+        }
+    }
+}
