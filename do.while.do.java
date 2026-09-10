db56dab919de2df0fe1abd18d2834a2a04e8809d
Narsing Narvate <narvatenarsing8@gmail.com>
2026-09-10 11:43:09 +0530

    Add Java do-while loop example

---
+++ do.while.do.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        do {
+            System.out.println("The output is: " + x);
+            x = x - 2;
+        } while (x >= 0);
+    }
+}
