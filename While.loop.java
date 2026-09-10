0c451bfa6f079b63552c89c1659a9c095c44196d
Narsing Narvate <narvatenarsing8@gmail.com>
2026-09-10 11:40:34 +0530

    Add Java while loop example

---
+++ While.loop.java
@@ -0,0 +1,14 @@
+public class WhileLoop {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 5) {
+            System.out.println("The output is " + i);
+            i++;
+        }
+
+        System.out.println("While loop is over");
+    }
+}
